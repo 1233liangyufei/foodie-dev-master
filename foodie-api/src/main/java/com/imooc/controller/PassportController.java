ef27e0e1b914f116300f02d46aceeec35b43e1ea
liangyufei <l957087142@gmail.com>
2021-04-27 20:31:01 +0800

    自定义响应数据结果，更加友好的返回值

--- foodie-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-api/src/main/java/com/imooc/controller/PassportController.java
@@ -2,8 +2,10 @@ package com.imooc.controller;
 
 import com.imooc.service.StuService;
 import com.imooc.service.UserService;
+import com.imooc.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -16,19 +18,19 @@ public class PassportController {
     private UserService userService;
 
     @GetMapping("/usernameIsExist")
-    public int usernameIsExist(@RequestParam String username){
+    public IMOOCJSONResult usernameIsExist(@RequestParam String username){
         //1.判断入参不能为空
         if(StringUtils.isBlank(username)){
-            return 500;
+            return IMOOCJSONResult.errorMsg("用户名不能为空");
         }
         //2。查找注册的用户名是否存在
         boolean isExist = userService.queryUsernameIsExist(username);
         //System.out.println(isExist+"isExist");
         if(isExist){
-            return 500;
+            return IMOOCJSONResult.errorMsg("该用户已存在");
         }
         //3.请求成功，用户名没有重复
-        return 200;
+        return IMOOCJSONResult.ok();
     }
 
 
