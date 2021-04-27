c0dfa7dce4559af02d400adc3512e2f3e1ce816b
liangyufei <l957087142@gmail.com>
2021-04-27 21:06:49 +0800

    创建用户的service方法

--- foodie-api/src/main/java/com/imooc/Application.java
+++ foodie-api/src/main/java/com/imooc/Application.java
@@ -11,7 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 // 扫描 mybatis 通用 mapper 所在的包
 @MapperScan(basePackages = "com.imooc.mapper")
 // 扫描所有包以及相关组件包
-@ComponentScan(basePackages = {"com.imooc"})
+@ComponentScan(basePackages = {"com.imooc","org.n3r.idworker"})
 @EnableTransactionManagement
 @EnableScheduling       // 开启定时任务
 public class Application {
--- foodie-pojo/src/main/java/com/imooc/pojo/bo/UserBO.java
+++ foodie-pojo/src/main/java/com/imooc/pojo/bo/UserBO.java
@@ -1,24 +1,17 @@
 package com.imooc.pojo.bo;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel(value = "用户对象BO", description = "从客户端，由用户传入的数据封装在此entity中")
 public class UserBO {
-
-    @ApiModelProperty(value = "用户名", name = "username", example = "imooc", required = true)
     private String username;
-    @ApiModelProperty(value = "密码", name = "password", example = "123456", required = true)
     private String password;
-    @ApiModelProperty(value = "确认密码", name = "confirmPassword", example = "123456", required = false)
     private String confirmPassword;
 
-    public String getUsername() {
-        return username;
+    public String getConfirmPassword() {
+        return confirmPassword;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
     }
 
     public String getPassword() {
@@ -29,11 +22,11 @@ public class UserBO {
         this.password = password;
     }
 
-    public String getConfirmPassword() {
-        return confirmPassword;
+    public String getUsername() {
+        return username;
     }
 
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
